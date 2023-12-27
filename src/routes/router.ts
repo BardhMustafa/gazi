2a28244012d2a6d335db8d833a21313491338474
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-12-28 00:12:07 +0100

    add routing

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import Ballina from "../pages/Ballina";
 
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Ballina />,
+    Component: Ballina
   },
 ]);
 
