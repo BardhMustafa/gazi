e36716c9c4d4592787a73b3e131bffaa7eb083ce
rronhyseni <rronihyseni@gmail.com>
2023-12-28 12:27:10 +0100

    Create shared Button component

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #e91b37;
+  color: white;
+  font-size: 1.6rem;
+  font-weight: 500;
+  padding: 1.5rem 3rem;
+  border: none;
+  margin-top: 2rem;
+  cursor: pointer;
+`;
