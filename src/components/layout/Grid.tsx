69c0c501780a9d3c544ea58c7d948fc5d159184f
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-12-28 14:03:18 +0100

    Grid layout for 3 columns full responsive

---
+++ src/components/layout/Grid.tsx
@@ -0,0 +1,118 @@
+import Grid from '@mui/material/Grid';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Container,
+  Box
+} from '@mui/material';
+
+const GridLayout = ({ title , backgroundColor}: { title?: string , backgroundColor?: string}) => {
+  return (
+    <Box bgcolor={backgroundColor}>
+      {title && (
+        <Container style={{display:'flex', justifyContent:'center', alignContent:'center'}}>
+          <Typography variant="h2" component="div" gutterBottom>
+            {title}
+          </Typography>
+        </Container>
+      )}
+      <Grid sx={{ flexGrow: 1 }} container>
+        <Grid item xs={12}>
+          <Grid
+            container
+            justifyContent="center"
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 10 }}
+          >
+            <Grid item>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    shjdabs
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    shjdabs
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Word of the Day
+                  </Typography>
+                  <Typography variant="h5" component="div">
+                    shjdabs
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                  </Typography>
+                  <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default GridLayout;
