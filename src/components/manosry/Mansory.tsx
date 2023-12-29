f0080fdd52fa02f62373ac109c0a482ebae3b719
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-12-29 21:35:33 +0100

    add showroom

--- src/components/manosry/Mansory.tsx
+++ src/components/manosry/Mansory.tsx
@@ -1,26 +1,30 @@
-import * as React from 'react';
+
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
+import Paper, { PaperProps } from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
 
-const heights = [300, 700, 300, 400, 300, 350, 450, 450, 500, 550, 600, 400];
 
-const Item = styled(Paper)(({ theme }) => ({
+interface ItemProps extends PaperProps {
+  src: string;
+}
+
+const Item = styled(Paper)<ItemProps>(({ theme, src }) => ({
   backgroundSize: 'cover',
+  backgroundImage: `url(${src})`,
   ...theme.typography.body2,
   padding: theme.spacing(0.5),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
-export default function Mansory() {
+export default function Mansory({heights, pics}: {heights: number[], pics: string[]}) {
   return (
     <Box>
       <Masonry columns={{ xs: 1, sm: 2, md: 3}} spacing={2}>
         {heights.map((height, index) => (
-          <Item key={index} sx={{ height }}>
-            {index + 1}
+          <Item key={index} sx={{ height }} src={pics[index]}>
+
           </Item>
         ))}
       </Masonry>
--- src/pages/Showroom.tsx
+++ src/pages/Showroom.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
 import Mansory from '../components/manosry/Mansory';
+import Pic1 from '../assets/showroom/pic1.jpg';
+import Pic2 from '../assets/showroom/pic2.jpg';
+import Pic3 from '../assets/showroom/pic3.jpg';
+import Pic4 from '../assets/showroom/pic4.jpg';
+import Pic5 from '../assets/showroom/pic5.jpg';
+import Pic6 from '../assets/showroom/pic6.jpg';
+import Pic7 from '../assets/showroom/pic7.jpg';
+import Pic8 from '../assets/showroom/pic8.jpg';
+import Pic9 from '../assets/showroom/pic9.jpg';
+import Pic10 from '../assets/showroom/pic10.jpg';
+import Pic11 from '../assets/showroom/pic11.jpg';
+import Pic12 from '../assets/showroom/pic12.jpg';
+import Pic13 from '../assets/showroom/pic13.jpg';
+import Pic14 from '../assets/showroom/pic14.jpg';
+import Box from '@mui/material/Box';
+import { Heading2 } from '../shared/components/Heading2';
+
+const heights = [300, 700, 300, 400, 300, 350, 450, 450, 500, 300, 400, 500, 600, 500];
+const pics = [Pic7, Pic3, Pic11, Pic1, Pic10, Pic2, Pic4, Pic14, Pic8, Pic13, Pic6, Pic9, Pic5, Pic12];
 
 const Showroom = () => {
   return (
-
-    <Mansory/>
+    <>
+      <Box display='flex' justifyContent='center' alignContent='center'>
+        <Heading2>Showroom</Heading2>
+      </Box>
+      <Mansory heights={heights} pics={pics}/>
+    </>
   );
 };
 
