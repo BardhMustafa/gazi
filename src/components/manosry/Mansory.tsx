ddd1d094e2158aa5ed7f483a1d2f1ea87b8f4896
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-12-29 20:19:56 +0100

    update mansory

--- src/components/manosry/Mansory.tsx
+++ src/components/manosry/Mansory.tsx
@@ -4,7 +4,7 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
 
-const heights = [200, 250, 300, 100, 150, 350, 400, 450, 500, 550, 600];
+const heights = [300, 700, 300, 400, 300, 350, 450, 450, 500, 550, 600, 400];
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundSize: 'cover',
@@ -17,7 +17,7 @@ const Item = styled(Paper)(({ theme }) => ({
 export default function Mansory() {
   return (
     <Box>
-      <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
+      <Masonry columns={{ xs: 1, sm: 2, md: 3}} spacing={2}>
         {heights.map((height, index) => (
           <Item key={index} sx={{ height }}>
             {index + 1}
