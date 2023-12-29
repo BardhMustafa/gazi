20eeff7cdf7d6674c79284afd0ce6f2fb1c787bb
BardhMustafa <50417505+BardhMustafa@users.noreply.github.com>
2023-12-29 23:23:19 +0100

    add factory

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -14,6 +14,7 @@ const Pvc = lazy(() => import('../pages/Products/Pvc'));
 const Alumin = lazy(() => import('../pages/Products/Alumin'));
 import { ErrorPage } from '../pages/ErrorPage';
 import Loader from '../shared/components/Loader';
+import Factory from '../pages/Factory';
 
 const router = createBrowserRouter([
   {
@@ -80,6 +81,14 @@ const router = createBrowserRouter([
         element: (
           <Suspense fallback={<Loader />}>
             <Showroom />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/factory',
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Factory />
           </Suspense>
         ),
       },
